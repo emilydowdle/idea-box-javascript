128129b65defff75bca0c263199b1ff1c5643778
Emily Dowdle <emily@editingemily.com>
2016-02-02 09:08:35 -0700

    Display ideas on index

--- app/assets/javascripts/home_index.js
+++ app/assets/javascripts/home_index.js
@@ -1,21 +1,23 @@
-$(document).ready(function) {
-  fetchIdeas
-}
+$(document).ready(function() {
+  fetchIdeas()
+})
 
 function fetchIdeas() {
+  var newestItemId = parseInt($(".post").last().attr("data-id"))
   var ideaUri = 'http://localhost:3000/api/v1/ideas'
 
   $.getJSON(ideaUri, function(data) {
-    $('idea-info').html('');
-    $.each(data.results, function (key, val) {
-      debugger
-      var title = data.title;
-      var body = data.body;
-      var quality = data.quality;
+    $('#idea-info').html('');
+    $.each(data, function (key, val) {
+      var title = val.title;
+      var body = val.body;
+      var quality = val.quality;
+
+      $('#idea-info').append('<h3>' + title + '</h3>' +
+      '<p><strong>Summary: </strong>' + body + '</p>' +
+      '<p><strong>Quality: </strong>' + quality + '</p>')
     })
 
-    // $('idea-info').append('<h3>' + title + '</h3>' +
-    // '<p><strong>Summary: </strong>' + + '</p>' +
-    // '<p><strong>Quality: </strong>' + + '</p>')
+
   })
 }
